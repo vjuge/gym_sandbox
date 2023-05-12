e43116c51dde307144918f6678142217cc3bd6e3
vjuge <vincent@chaindatax.com>
2023-05-12 09:56:58 +0200

    feat(mountain_car): trained model working only 5000 epochs !

--- MountainCar_Train.py
+++ MountainCar_Train.py
@@ -8,24 +8,41 @@ env = gym.make('MountainCar-v0')
 n_grid = 10
 # grid over observations[0] and actions[1]
 gridspace = {}
+# empty qtable
+q_table = np.zeros((n_grid, n_grid, env.action_space.n))
 # learning rate
 alpha = 0.1
 # discount factor
 gamma = 0.95
 
 # number of episodes of training
-epochs = 25000
+epochs = 5000
 
 # exploration/exploitation rate : 1 = 100% exploration, 0 = 100% exploitation
 # 'epsilon greedy' strategy : epsilon is very high at the beginning, then decreases over time
 epsilon = 1.
 
-epsilon_min = 0.2
+epsilon_min = 0.1
 start_epsilon_decaying = 1000
 end_epsilon_decaying = epochs // 2
 epsilon_decay_value = epsilon / (end_epsilon_decaying - start_epsilon_decaying)
 
+TRAIN = False
 PREDICT = True
+
+
+def init():
+    """
+    Initialize gridspace and q_table
+    :return:
+    """
+    print("**** Initialization ****")
+    global gridspace, q_table
+    for ii in range(env.observation_space.shape[0]):
+        gridspace[ii] = np.linspace(env.observation_space.low[ii], env.observation_space.high[ii], n_grid)
+
+    q_table = np.random.uniform(low=-2, high=2, size=(n_grid, n_grid, env.action_space.n))
+
 
 # define a function that makes it easy to get our discrete indices from an observation
 def to_discrete_state(observation: np.ndarray(2, )):
@@ -69,60 +86,44 @@ def explore_or_exploit(q_value):
     return action
 
 
-def main():
+def train():
     """
-    Main function
-
-    Description and details of this challenge are available here: https://gymnasium.farama.org/environments/classic_control/mountain_car/
-
+    Train the agent
+    :return:
     """
-    global epsilon
-    print("**** Initialization ****")
-    # print("action_space: ", env.action_space)
-    # print("action_space_dim: ", env.action_space.n)
-
-    # observation space
-    print("observation_space: ", env.observation_space)
-    # initial state observation
-    initial_observation = env.reset()
-    print("initial observations: || position: ", initial_observation[0][0], " || velocity: ", initial_observation[0][1])
-
-    # build grid space = discretize the observation space
-    for ii, s in enumerate(zip(env.observation_space.low, env.observation_space.high)):
-        gridspace[ii] = np.round(np.linspace(s[0], s[1], n_grid), 2)
-    print("using grid space (pos, vel): ", gridspace)
-
-    # discretize initial observation to get the initial state
-    discrete_state = to_discrete_state(initial_observation[0])
-    print("initial state :", discrete_state)
-
-    # build q_table = initialize the q_table
-    q_table = np.random.uniform(low=-2, high=0, size=(n_grid, n_grid, env.action_space.n))
-    # q_table = np.zeros([n_grid, n_grid, env.action_space.n])
-
+    global epsilon, q_table
     print("**** Training ****")
     for episode in range(epochs):
+        discrete_state = to_discrete_state(env.reset()[0])
         done = False
         while not done:
-            action = explore_or_exploit(q_table[discrete_state])
+
+            # choose action
+            if np.random.random() > epsilon:
+                # exploit
+                action = np.argmax(q_table[discrete_state])
+            else:
+                # explore
+                action = np.random.randint(0, env.action_space.n)
 
             observation, reward, terminated, truncated, info = env.step(action)
+            new_discrete_state = to_discrete_state(observation)
 
             # if observation[0] >= env.goal_position:
             #     reward = 1
             # print("obs: ", obs)
 
-            discrete_state = to_discrete_state(observation)
-
             # update q_table using Bellman formula
-            max_future_q = np.max(q_table[discrete_state])
+            max_future_q = np.max(q_table[new_discrete_state])
             current_q = q_table[discrete_state + (action,)]
-            # print("alpha: ", alpha, "gamma: ", gamma, "reward: ", reward)
-            new_q = (1 - alpha) * current_q + alpha * (reward + gamma * max_future_q)
+            new_q = (1 - alpha) * current_q + alpha * (reward + (gamma * max_future_q))
             # print("max_future_q: ", max_future_q, "current_q: ", current_q, "new_q: ", new_q)
             q_table[discrete_state + (action,)] = new_q
 
+            discrete_state = new_discrete_state
+
             done = terminated or truncated  # if terminated or truncated, then the episode is done
+
         # lower epsilon on next epoch
         if epsilon > epsilon_min:
             epsilon -= epsilon_decay_value
@@ -133,19 +134,40 @@ def main():
 
     print("final q_table: ")
     print(q_table)
-    # np.save("q_table", q_table)
+    np.save("q_table", q_table)
+
+
+def predict():
+    """
+    Predict the agent
+    :return:
+    """
+    print("**** Predict ****")
+    q_table = np.load("q_table.npy")
+    env = gym.make('MountainCar-v0', render_mode="human")
+    # env_test = gym.make('MountainCar-v0')
+    discrete_state = to_discrete_state(env.reset(options={}, seed=42)[0])
+    done = False
+    while not done:
+        action_index = np.argmax(q_table[discrete_state])
+        observation, reward, terminated, truncated, info = env.step(action_index)
+        discrete_state = to_discrete_state(observation)
+        done = terminated or truncated
+
+
+def main():
+    """
+    Main function
+
+    Description and details of this challenge are available here: https://gymnasium.farama.org/environments/classic_control/mountain_car/
+
+    """
+    init()
+    if TRAIN:
+        train()
 
     if PREDICT:
-        print("**** Predict ****")
-        env_test = gym.make('MountainCar-v0', render_mode="human")
-        # env_test = gym.make('MountainCar-v0')
-        discrete_state = to_discrete_state(env_test.reset(options={})[0])
-        done = False
-        while not done:
-            action_index = np.argmax(q_table[discrete_state])
-            observation, reward, terminated, truncated, info = env_test.step(action_index)
-            discrete_state = to_discrete_state(observation)
-            done = terminated or truncated
+        predict()
 
 
 if __name__ == "__main__":
